7c66426c45ead855f6554dabfb5d79fd40e511e2
ADMINISTRATOR\Administrator <duc.bui8@gmail.com>
2017-04-27 10:42:44 +0700

    no message

--- sheep.py
+++ sheep.py
@@ -2,19 +2,9 @@
 ##built for remove same maximum values
 def samemax(a):
     a.sort()
-    b=0
-    temp=0
-    while a[b-1]==a[b-2]:
-        del a[b-1]
-        a.sort()
-        b=len(a)
-        temp=a[b-1]
-    if a[len(a)-1]==temp:
-        del a[len(a)-1]
-        a.sort()
-    else:
-        a[len(a)-1]==max(a)
-        del(a[len(a)-1])
+    temp=max(a)
+    while a[len(a)-1]==temp:
+        a[len(a)-1]=8
         a.sort()
 #main program
 def sheepnotice(a,n):
@@ -23,12 +13,8 @@ def sheepnotice(a,n):
         print('MONTH',i+1)
         mx=max(a)
         print('my biggest sheep:',mx,'let fukin cut its hair')
-        count1=len(a)
         #shearing
         samemax(a)
-        count2=len(a)
-        for _ in range(0,count1-count2):
-            a.append(8)
         print('my sheeps after being sheard:',a)
         a=list(map(lambda x:x+50,a))
         print('after growth',a)
@@ -41,6 +27,6 @@ def bored(a,s):
     print('my total fukin money  $',total*s)     
 print('Hello my name is unknown and there is my sheep sizes xd')
 #nhap 1 day size cuu vao day
-size=[500,7,90,24,500,75,90]
+size=[100,100,100,200,300,400,500,600,600,600]
 sheepnotice(size,4)
 
